4cabc7ae7b0244fa75aa0f1b6d5708f8e1db1373
NileCoder211 <nilecoder8@gmail.com>
2026-09-24 17:17:36 +0300

    Remove duplicate import of 'path' in server.js
    Removed redundant import statement for 'path' to clean up code.

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import path from "path";
 import cookieParser from "cookie-parser";
 import cors from "cors"
-import path from "path";
 import { fileURLToPath } from "url";
 
 
